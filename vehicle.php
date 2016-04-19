4c42257ea894458c93dcd3f9ebd4531702cf3000
getwiz <pointfuse@gmail.com>
2016-04-19 09:04:27 +0700

    Create vehicle.php

---
+++ vehicle.php
@@ -0,0 +1,43 @@
+<?php
+class vehicle{
+/*** define public properties ***/
+
+/*** the color of the vehicle ***/
+public $color;
+
+/*** the number of doors ***/
+public $num_doors;
+
+/*** the price of the vehicle ***/
+public $price;
+
+/*** the shape of the vehicle ***/
+public $shape;
+
+/*** the brand of vehicle ***/
+public $brand;
+
+/*** the constructor ***/
+public function __construct(){
+  echo 'About this Vehicle.<br />';
+}
+
+/*** define some public methods ***/
+
+/*** a method to show the vehicle price ***/
+public function showPrice(){
+  echo 'This vehicle costs '.$this->price.'.<br />';
+}
+
+/*** a method to show the number of doors ***/
+public function numDoors(){
+  echo 'This vehicle has '.$this->num_doors.' doors.<br />';
+}
+
+/*** method to drive the vehicle ***/
+public function drive(){
+  echo 'VRRROOOOOOM!!!';
+}
+
+} /*** end of class ***/
+?>
