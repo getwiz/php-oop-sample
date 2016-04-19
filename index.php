608a5bb8cd020bad4389dc862c4af456a7f66c00
getwiz <pointfuse@gmail.com>
2016-04-19 09:04:59 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/*** create a new vehicle object ***/
+$vehicle = new vehicle;
+
+/*** the brand of vehicle ***/
+$vehicle->brand = 'Porsche';
+
+/*** the shape of vehicle ***/
+$vehicle->shape = 'Coupe';
+
+/*** the color of the vehicle ***/
+$vehicle->color = 'Red';
+
+/*** number of doors ***/
+$vehicle->num_doors = 2;
+
+/*** cost of the vehicle ***/
+$vehicle->price = 100000;
+
+/*** call the showPrice method ***/
+$vehicle->showPrice();
+
+/*** call the numDoors method ***/
+$vehicle->numDoors();
+
+/*** drive the vehicle ***/
+$vehicle->drive();
+
+?>
